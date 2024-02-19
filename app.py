b38e334b1e6421450ab90c962c9c9d34de787915
irfanakbari <irfanakbarihabibi18@gmail.com>
2024-02-20 06:40:09 +0700

    init

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import vertexai
 from flask_socketio import send, SocketIO
-from vertexai.language_models import ChatModel, InputOutputTextPair
+from vertexai.language_models import ChatModel, InputOutputTextPair, ChatMessage
 from flask import Flask
 from flask_cors import CORS  # Import the CORS extension
 
@@ -9,19 +9,48 @@ CORS(app)
 socketio = SocketIO(app)
 socketio.init_app(app, cors_allowed_origins="*")
 
-vertexai.init(project="summer-surface-403502", location="asia-southeast1")
-chat_model = ChatModel.from_pretrained("chat-bison")
+vertexai.init(project="halalens-413617", location="asia-southeast1")
+chat_model = ChatModel.from_pretrained("chat-bison-32k")
 parameters = {
     "candidate_count": 1,
     "max_output_tokens": 1024,
-    "temperature": 0.2,
-    "top_p": 0.8,
-    "top_k": 40
+    "temperature": 0.9,
+    "top_p": 1,
+    "top_k": 40,
 }
+# safety_settings = {
+#     generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_NONE,
+#     generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.
+#     BLOCK_NONE,
+#     generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.
+#     BLOCK_NONE,
+#     generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_NONE,
+# }
+message_history=[]
 chat = chat_model.start_chat(
-    context="""Anda adalah Mr.SehatIn, chatbot layanan pengguna untuk SehatIn App Anda hanya menjawab pertanyaan 
-    pelanggan tentang dunia kesehatan, terutama makanan sehat, pola hidup sehat, saran kesehatan, resep makanan 
-    sehat. hindari membahas diluar konteks kesehatan.""",
+    # message_history=message_history,
+    context="""nama anda adalah HalalBot, chatbot layanan pengguna untuk Halalens App semua respon harus bahasa 
+    indonesia. Anda hanya menjawab pertanyaan pelanggan terkait pengertian halal haram, terutama informasi apa itu 
+    makanan halal, pola makan halal, resep makanan halal. hindari membahas diluar konteks, dan tolak jika pengguna 
+    bertanya apakah makanan tertentu halal atau haram suatu bahan""",
+    examples=[
+        InputOutputTextPair(
+            input_text="""buatkan kodingan program""",
+            output_text="""Maaf saya tidak bisa menjawab pertanyaan tersebut karena diluar konteks"""
+        ),
+        InputOutputTextPair(
+            input_text="""apakah pewarna haram""",
+            output_text="""Maaf saya tidak bisa menjawab pertanyaan tersebut"""
+        ),
+        InputOutputTextPair(
+            input_text="""bagaimana cara memasak nasi yang benar""",
+            output_text="""Maaf saya tidak bisa menjawab pertanyaan tersebut karena diluar konteks"""
+        ),
+        InputOutputTextPair(
+            input_text="""saran makanan halal dengan bahan tepung""",
+            output_text="""anda mungkin bisa membuat bakwan dengan bahan dasar tepung"""
+        )
+    ],
 )
 
 
@@ -30,6 +59,9 @@ chat = chat_model.start_chat(
 def handle_message(msg):
     response = chat.send_message(msg, **parameters)
     # Send the response back to the client that sent the original message
+    # message_history.insert(ChatMessage(
+    #         content="""Maaf saya tidak bisa menjawab pertanyaan tersebut karena diluar konteks""",
+    #         author=response.author))
     send(str(response.text))
 
 
